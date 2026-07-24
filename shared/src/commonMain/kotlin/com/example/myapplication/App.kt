276db44a3db34e4d8aa793608e3b15454a663d47
jonathan kee <jonathankee99@gmail.com>
2026-07-24 20:09:03 +0800

    Using my first composable

--- shared/src/commonMain/kotlin/com/example/myapplication/App.kt
+++ shared/src/commonMain/kotlin/com/example/myapplication/App.kt
@@ -1,47 +1,34 @@
 package com.example.myapplication
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import org.jetbrains.compose.resources.painterResource
-
-import myapplication.shared.generated.resources.Res
-import myapplication.shared.generated.resources.compose_multiplatform
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.composable.AfterClickMe
+import com.example.myapplication.composable.ClickMe
+import com.example.myapplication.composable.ResponsiveLayout
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
+        Column() {
             Button(onClick = { showContent = !showContent }) {
-                Text("Click me! Testing")
+                ClickMe("Testing")
             }
+
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+                Column() {
+                    // The modifier is optional and already has default
+                    AfterClickMe("Jonathan", Modifier.padding(16.dp))
+                    ResponsiveLayout()
                 }
             }
         }
--- shared/src/commonMain/kotlin/com/example/myapplication/composable/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/myapplication/composable/Greeting.kt
@@ -2,6 +2,8 @@ package com.example.myapplication.composable
 
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,15 +13,29 @@ class Greeting {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ClickMe(name: String) {
+    Text(text = "This composable in inside Greeting.kt")
+}
+
+@Composable
+fun AfterClickMe(name: String, modifier: Modifier = Modifier) {
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
 }
 
 @Composable
 fun ResponsiveLayout() {
     FlowRow {
         Text(text = "Item 1")
+    }
+    FlowRow{
         Text(text = "Item 2")
+    }
+    FlowRow{
         Text(text = "Item 3")
     }
 }

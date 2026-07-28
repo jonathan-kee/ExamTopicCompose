7c70f59be6d6b47e80a5d6387f2f46ec173f9310
jonathan kee <jonathankee99@gmail.com>
2026-07-28 17:12:15 +0800

    remove old dependency

--- shared/src/commonMain/kotlin/com/example/myapplication/App.kt
+++ shared/src/commonMain/kotlin/com/example/myapplication/App.kt
@@ -4,7 +4,6 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
